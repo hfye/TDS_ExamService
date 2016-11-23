16e3caa4bcb43160da94f66661d64739450b5766
Ernie Munoz <emunoz@fairwaytech.com>
2016-11-23 12:26:09 -0800

    Changes related to refactoring of Algorithm enum in AssessmentService

--- service/src/test/java/tds/exam/builder/SegmentBuilder.java
+++ service/src/test/java/tds/exam/builder/SegmentBuilder.java
@@ -2,6 +2,7 @@ package tds.exam.builder;
 
 import java.util.List;
 
+import tds.assessment.Algorithm;
 import tds.assessment.ItemProperty;
 import tds.assessment.Segment;
 
@@ -10,7 +11,7 @@ import static java.util.Collections.singletonList;
 public class SegmentBuilder {
     private String key = "(SBAC_PT)IRP-Perf-ELA-3-Summer-2015-2016";
     private String segmentId = "IRP-Perf-ELA-3";
-    private String selectionAlgorithm = "fixedform";
+    private Algorithm selectionAlgorithm = Algorithm.FIXED_FORM;
     private float startAbility = 0;
     private String subjectName = "ENGLISH";
     private String assessmentKey = "(SBAC_PT)IRP-Perf-ELA-3-Summer-2015-2016";
@@ -37,7 +38,7 @@ public class SegmentBuilder {
         return this;
     }
 
-    public SegmentBuilder withSelectionAlgorithm(String selectionAlgorithm) {
+    public SegmentBuilder withSelectionAlgorithm(Algorithm selectionAlgorithm) {
         this.selectionAlgorithm = selectionAlgorithm;
         return this;
     }
