e868b70528db211a5236e3ce17718d5abff6929a
Mike Trupkin <mtrupkin@fairwaytech.com>
2017-01-13 18:28:37 -0800

    add health check endpoint test

---
+++ service/src/test/java/tds/exam/health/HealthIndicatorTest.java
@@ -0,0 +1,30 @@
+package tds.exam.health;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HealthIndicatorTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHealthCheck() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/health").accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(containsString("status")));
+    }
+}
